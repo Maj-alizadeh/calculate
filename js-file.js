c54791228d7c17f6eab815dd1cec2f3dd9b78979
maj-alizadeh <majed.alizadeh@gmail.com>
2022-11-05 23:47:08 +0330

    Add operators functions

---
+++ js-file.js
@@ -0,0 +1,22 @@
+function add(number1,number2) {
+    return number1+number2;
+}
+
+function Subtract(number1,number2) {
+    return number1 - number2;
+}
+
+function multiply(number1,number2) {
+    return number1 * number2;
+}
+
+function divide(number1,number2) {
+    return number1 / number2;
+}
+
+function power(number1,number2) {
+    return Math.pow(number1,number2);
+}
+
+
+    
